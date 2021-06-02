b63f24fc0d97bc1ae99dfdb323234c209a109606
Volodymyr Ruskov <vova.ruskov@gmail.com>
2021-06-02 17:46:10 +0300

    fix
    устранение "рывка" размера текста при первом изменении положения ползунка

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const inputRef = document.querySelector('#font-size-control');
 const spanRef = document.querySelector('#text');
 
+spanRef.style.fontSize = `${inputRef.value}px`;
+
 const onInputValueChange = event => {
     const value = event.currentTarget.value;
     spanRef.style.fontSize = `${value}px`;
